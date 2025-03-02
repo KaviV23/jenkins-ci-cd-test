02843d21e41209199c5408bb4b96678edafc58b9
Kaviraj Vijayanthiran <129849114+KaviV23@users.noreply.github.com>
2025-03-02 23:50:02 +0800

    Test commit to trigger Jenkins #2

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,6 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-
       <p>Pipelined from GitHub to VPS via Jenkins</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
